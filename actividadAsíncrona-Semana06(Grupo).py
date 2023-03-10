eeccff5f09b2daa41391c3b3741aa60381858123
wenespinoza12 <127539668+wenespinoza12@users.noreply.github.com>
2023-03-10 12:26:38 -0600

    Add files via upload

---
+++ actividadAsíncrona-Semana06(Grupo).py
@@ -0,0 +1,40 @@
+print("Actividad Asíncrona Semana 06")
+print("")
+print("SUMA")
+numUno = 15
+numDos= 3
+numTres = 12
+resultSum = numUno + numDos + numTres
+print ("El resultado de la suma es: ", str(resultSum))
+print("")
+print("RESTA")
+num1 = 12
+num2 = 9
+num3 = 15
+num4 = 7
+resultRes = num1 - num2 - num3 - num4
+print("El resultado de la resta es: ", str(resultRes))
+print("")
+print("MULTIPLICACIÓN")
+numUn = 2
+numDo = 4
+numTre = numUno * numDos
+resultMulti = numTre * numDos * num2
+print("El resultado de la multiplicación es: ", str(resultMulti))
+print("")
+print("EXPONENTE")
+resultExp = numUn ** numDo
+print("El resultado del exponente es: ", str(resultExp))
+print("")
+print("MÓDULO")
+resultMod = numUno % num1
+print("El resultado de módulo es: ", str(resultMod))
+print("")
+print("DIVISIÓN")
+resultDiv = resultExp / resultMod
+print("El resultado de la división es:", str(resultDiv))
+print("")
+print("DIVISIÓN ENTERA")
+resultDivE = resultExp // resultMod
+print("El resultado de la división entera es: ", str(resultDivE))
+
